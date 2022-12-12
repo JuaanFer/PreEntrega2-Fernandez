fb4b766fea2a66e2abf5e2f19c310c1b1ee44fbc
Juan Pablo Fernandez <juan.pablo.fernandez@outlook.com>
2022-12-12 04:54:23 -0300

    editar pedido

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const Plato3 = new Plato(3,'Hambugesa completa',1200,"https://encrypted-tbn0.gst
 const Plato4 = new Plato(4,'Ensalada',750,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwC9T0wgEqqcpO7WSFPn7sfMZSCd-QJGMJmQ&usqp=CAU")
 
 const platos = [Plato1,Plato2,Plato3,Plato4]
-const pedido = JSON.parse(localStorage.getItem("pedido")) || []
+let pedido = JSON.parse(localStorage.getItem("pedido")) || []
 
 const platosDiv = document.querySelector("#platosDiv")
 
-platos.forEach(plato => {
+platos.forEach((plato) => {
     platosDiv.innerHTML +=`
     <div id="plato${plato.id}" class="card prodCard">
         <div class="card-body">
             <img src="${plato.imagen}" class="card-img-top" alt="">
             <h5 class="card-title">${plato.nombre}</h5>
             <p class="card-text">$${plato.precio}</p>
-            <button id=${plato.id} class="btn btn-dark">Agregar</button>
+            <button id=${plato.id} class="btn btn-dark btn-agregar">Agregar</button>
         </div>
     </div>
     `
 })
 
-const btnAgregar = document.querySelectorAll(".btn-dark")
+const btnAgregar = document.querySelectorAll(".btn-agregar")
 
 btnAgregar.forEach(boton => {
     boton.onclick = () =>{
@@ -38,14 +38,75 @@ btnAgregar.forEach(boton => {
         const platoCarrito = {...platoSelect,cantidad:1}
 
         const indexCarrito = pedido.findIndex(plato => plato.id === platoCarrito.id)
-        if(indexCarrito === -1){
-            pedido.push(platoCarrito)
-        }else{
-            pedido[indexCarrito].cantidad += 1
-        }
-        localStorage.setItem("pedido",JSON.stringify(pedido))
+
+        indexCarrito === -1 ? pedido.push(platoCarrito) : pedido[indexCarrito].cantidad++
+        
+        agregarStorage()
+
+        Toastify({
+            text: "Agregado al pedido",
+            duration: 1500,
+            gravity: "bottom",
+            stopOnFocus: false,
+            style: {
+                background: "green"
+            },
+            avatar: "https://cdn.icon-icons.com/icons2/1555/PNG/96/fast-food-icons-freeburger_107425.png"
+        }).showToast()
     }
 });
+
+const btnPedido = document.querySelector("#btnMostrarPedido")
+const pedidoDiv = document.querySelector(".modal-body")
+
+btnPedido.onclick = () =>{
+    mostrarPedido()
+}
+
+function mostrarPedido(){
+    
+    pedidoDiv.innerHTML = ""
+    pedido.forEach((item) => {
+        const {id,nombre, precio, imagen} = item
+        pedidoDiv.innerHTML += `
+         <div class="modal-contenedor">
+            <div>
+            <img class= "img-fluid img-carrito" src="${imagen}"/>
+            </div>
+            <div>
+           <p>Producto: ${nombre}</p>
+            <p>Precio: ${precio}</p>
+           <button onclick="eliminarPlato(${id})" class= "btn btn-danger btn-eliminar">Eliminar</button>
+           </div>
+         </div>
+        `
+    })
+    if(pedido.length===0){
+        pedidoDiv.innerHTML = `<h5>Aun no tienes ningun plato seleccionado</h5>`
+    }
+}
+
+function eliminarPlato(id){
+    const platoId = id
+    pedido = pedido.filter((plato) => plato.id !== platoId)
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1200,
+    })
+      
+    Toast.fire({
+        icon: 'success',
+        title: 'Plato eliminado'
+    })
+    mostrarPedido()
+    agregarStorage()
+}
+
+function agregarStorage(){
+    localStorage.setItem("pedido", JSON.stringify(pedido))
+}
 
 const finalizarPedido = document.querySelector("#finalizar")
 finalizarPedido.onclick = () =>{
