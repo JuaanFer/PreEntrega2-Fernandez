8f5213faa02d136b38b8a27a3740bf5a469128bf
Juan Pablo Fernandez <juan.pablo.fernandez@outlook.com>
2022-12-15 03:52:45 -0300

    ultimos cambios

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-class Plato {
-    constructor(id, nombre, precio,imagen) {
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.imagen = imagen
-    }
-}
-const Plato1 = new Plato(1,'Hambugesa simple',800,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqFMxYwZ2y1umes0CPDj7adLxZ7Puah3smfQ&usqp=CAU")
-const Plato2 = new Plato(2,'Hambugesa con queso',1000,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwowx19GutwgVwPnIbND0SS8BOhu0vyCxuaw&usqp=CAU")
-const Plato3 = new Plato(3,'Hambugesa completa',1200,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN0ImJB1JBxk0MtZqniJjJbHBgeP3_nalOTQ&usqp=CAU")
-const Plato4 = new Plato(4,'Ensalada',750,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwC9T0wgEqqcpO7WSFPn7sfMZSCd-QJGMJmQ&usqp=CAU")
-
-const platos = [Plato1,Plato2,Plato3,Plato4]
 let pedido = JSON.parse(localStorage.getItem("pedido")) || []
 
-const platosDiv = document.querySelector("#platosDiv")
+const cargaProductos= async () =>{
+    const response = await fetch("./json/platos.json")
+    const platos = await response.json()
+    return platos
+}
 
-platos.forEach((plato) => {
-    platosDiv.innerHTML +=`
-    <div id="plato${plato.id}" class="card prodCard">
-        <div class="card-body">
-            <img src="${plato.imagen}" class="card-img-top" alt="">
-            <h5 class="card-title">${plato.nombre}</h5>
-            <p class="card-text">$${plato.precio}</p>
-            <button id=${plato.id} class="btn btn-dark btn-agregar">Agregar</button>
+cargaProductos().then(platos =>{
+
+    const platosDiv = document.querySelector("#platosDiv")
+
+    platos.forEach(({id,img,nombre,precio}) => {
+        platosDiv.innerHTML +=`
+        <div id="plato${id}" class="card prodCard">
+            <div class="card-body">
+                <img src="${img}" class="card-img-top imgCard" alt="">
+                <h5 class="card-title">${nombre}</h5>
+                <p class="card-text">$${precio}</p>
+                <button id=${id} class="btn btn-dark btn-agregar">Agregar</button>
+            </div>
         </div>
-    </div>
-    `
+        `
+    })
+
+    const btnAgregar = document.querySelectorAll(".btn-agregar")
+
+    btnAgregar.forEach(boton => {
+        boton.onclick = () =>{
+            const platoSelect = platos.find(plato =>plato.id === parseInt(boton.id))
+            
+            const platoCarrito = {...platoSelect,cantidad:1}
+
+            const indexCarrito = pedido.findIndex(plato => plato.id === platoCarrito.id)
+
+            indexCarrito === -1 ? pedido.push(platoCarrito) : pedido[indexCarrito].cantidad++
+            
+            agregarStorage()
+
+            Toastify({
+                text: "Agregado al pedido",
+                duration: 1500,
+                gravity: "bottom",
+                stopOnFocus: false,
+                style: {
+                    background: "green"
+                },
+                avatar: "https://cdn.icon-icons.com/icons2/1555/PNG/96/fast-food-icons-freeburger_107425.png"
+            }).showToast()
+            totalPedido.innerText = `$${calcularTotal()}` 
+        }
+    });
+
 })
-
-const btnAgregar = document.querySelectorAll(".btn-agregar")
-
-btnAgregar.forEach(boton => {
-    boton.onclick = () =>{
-        const platoSelect = platos.find(plato =>plato.id === parseInt(boton.id))
-        
-        const platoCarrito = {...platoSelect,cantidad:1}
-
-        const indexCarrito = pedido.findIndex(plato => plato.id === platoCarrito.id)
-
-        indexCarrito === -1 ? pedido.push(platoCarrito) : pedido[indexCarrito].cantidad++
-        
-        agregarStorage()
-
-        Toastify({
-            text: "Agregado al pedido",
-            duration: 1500,
-            gravity: "bottom",
-            stopOnFocus: false,
-            style: {
-                background: "green"
-            },
-            avatar: "https://cdn.icon-icons.com/icons2/1555/PNG/96/fast-food-icons-freeburger_107425.png"
-        }).showToast()
-    }
-});
 
 const btnPedido = document.querySelector("#btnMostrarPedido")
 const pedidoDiv = document.querySelector(".modal-body")
@@ -67,23 +64,40 @@ function mostrarPedido(){
     
     pedidoDiv.innerHTML = ""
     pedido.forEach((item) => {
-        const {id,nombre, precio, imagen} = item
+        const {id,nombre, precio, img, cantidad} = item
         pedidoDiv.innerHTML += `
-         <div class="modal-contenedor">
+        <div class="modal-contenedor">
             <div>
-            <img class= "img-fluid img-carrito" src="${imagen}"/>
+            <img class= "img-fluid img-carrito" src="${img}"/>
             </div>
             <div>
-           <p>Producto: ${nombre}</p>
+            <p>Producto: ${nombre}</p>
             <p>Precio: ${precio}</p>
-           <button onclick="eliminarPlato(${id})" class= "btn btn-danger btn-eliminar">Eliminar</button>
-           </div>
-         </div>
+            <p>Cantidad: ${cantidad}</p>
+            <button onclick= "eliminarPlato(${id})"class="btn btn-danger btn-eliminar">Eliminar</button>
+            </div>
+        </div>
         `
     })
     if(pedido.length===0){
         pedidoDiv.innerHTML = `<p>Aun no has agregado nada...</p>`
+        totalPedido.innerText = ""
+    }else{
+        totalPedido.innerText = `$${calcularTotal()}` 
     }
+    calcularTotal()
+    
+}
+
+const totalPedido = document.querySelector("#totalPedido")
+
+calcularTotal = () =>{
+    const pedidoTotal = pedido.map(plato => plato.precio * plato.cantidad)
+    let precioTotal = 0
+    pedidoTotal.forEach(precio => {
+        precioTotal += precio
+    });
+    return precioTotal
 }
 
 function eliminarPlato(id){
@@ -95,26 +109,78 @@ function eliminarPlato(id){
         showConfirmButton: false,
         timer: 1200,
     })
-      
+    
     Toast.fire({
         icon: 'success',
         title: 'Plato eliminado'
     })
     mostrarPedido()
     agregarStorage()
+    if(pedido.length!==0){
+        totalPedido.innerText = `$${calcularTotal()}` 
+    }
+}
+
+const vaciarPedido = document.querySelector(".btnVaciar")
+vaciarPedido.onclick = () =>{
+    if(pedido.length!==0){
+        pedido.length = []
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1200,
+        })
+        Toast.fire({
+            icon: 'success',
+            title: 'Pedido vaciado'
+        })
+    }
+    mostrarPedido()
+    agregarStorage()
+    
+    totalPedido.innerText = ""
 }
 
 function agregarStorage(){
     localStorage.setItem("pedido", JSON.stringify(pedido))
 }
-
-const finalizarPedido = document.querySelector("#finalizar")
-finalizarPedido.onclick = () =>{
-    const pedidoTotal = pedido.map(plato => plato.precio * plato.cantidad)
-    let precioTotal = 0
-    pedidoTotal.forEach(precio => {
-        precioTotal += precio
-    });
-    console.log(pedido)
-    console.log(precioTotal)
+const btnPagar = document.querySelector("#btnPagar")
+btnPagar.onclick = () =>{
+    if(pedido.length===0){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Aun no tienes nada en tu pedido!',
+          })
+    }else{
+        Swal.fire({
+            icon: 'success',
+            title: 'Muchas Gracias',
+            text: 'Esperamos que disfrutes tu pedido',
+            showConfirmButton: false,
+          })
+        localStorage.clear()
+        setTimeout(()=>{
+            location.href = "index.html"
+        },3000)
+           
+    }
 }
+
+const invertal = setInterval(()=>{
+    if(pedido.length !== 0){
+        Swal.fire({
+            title: 'Recuerda que tienes platos agregados a tu pedido',
+            showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+    }else{
+        clearInterval(invertal)
+    }
+
+},300000)
